4d88f4f372142cf13cd6840d1154f761018f5b8a
shubxdoc <129772728+shubxdoc@users.noreply.github.com>
2024-08-13 17:49:45 +0000

    03 p1 file submit

---
+++ 01basics/03_conversionOp.js
@@ -0,0 +1,37 @@
+let score = "33abc"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN
+
+
+// "33" => 33 
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// ------------------------------------------------------------------------- //
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1=> true ; 0 => false
+// "" => false 
+// "abc" => true 
+
+// -------------------------------------------------------------------------- //
+
+let someNum = 123
+
+let stringSomeNum = String(someNum)
+
+console.log(typeof stringSomeNum);
+console.log(stringSomeNum);
