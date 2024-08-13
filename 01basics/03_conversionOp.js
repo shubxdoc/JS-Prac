e8a398c2a28c3cdd1762eeae5908ea0f08847e09
shubxdoc <129772728+shubxdoc@users.noreply.github.com>
2024-08-13 18:01:29 +0000

    config changes

--- 01basics/03_conversionOp.js
+++ 01basics/03_conversionOp.js
@@ -14,13 +14,13 @@ let score = "33abc"
 
 // ------------------------------------------------------------------------- //
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1=> true ; 0 => false
@@ -29,9 +29,27 @@ console.log(booleanIsLoggedIn);
 
 // -------------------------------------------------------------------------- //
 
-let someNum = 123
+// let someNum = 123
 
-let stringSomeNum = String(someNum)
+// let stringSomeNum = String(someNum)
 
-console.log(typeof stringSomeNum);
-console.log(stringSomeNum);
+// console.log(typeof stringSomeNum);
+// console.log(stringSomeNum);
+
+
+
+
+// ************************************** Operations ************************************** //
+
+let value = 3
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+
+let str1 = 'Hello'
+
+
+
+
