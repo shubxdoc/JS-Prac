7b27e31b4a8e3c66793fcd481f9080e5ac03a55b
shubxdoc <129772728+shubxdoc@users.noreply.github.com>
2024-08-14 16:16:44 +0000

    05 stack and heap concept added

--- 01basics/05_datatypes-Summary.js
+++ 01basics/05_datatypes-Summary.js
@@ -3,29 +3,59 @@
 // Primitive
 // 7 types: String, Num, Boolean, null, undefined, Symbol, BigInt(scientific values)
 
-const value = 100;
-const valueD = 100.3;
+// const value = 100;
+// const valueD = 100.3;
 
-const isLoggedIn = false;
-const outSideTemp = null;
-let userEmail;
+// const isLoggedIn = false;
+// const outSideTemp = null;
+// let userEmail;
 
-const id = Symbol("123");
-const anotherId = Symbol("123");
-console.log(id === anotherId); //false
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// console.log(id === anotherId); //false
 
-const bigNumber = 1324567890123123n;
+// const bigNumber = 1324567890123123n;
 
 // Reference (Non - Primative) -> inka datatype object aata hai
 // Array, Objects, Functions
 
-const heros = ["batman", "superman", "ironman"];
+// const heros = ["batman", "superman", "ironman"];
 
-let myObj = {
-  name: "Abc",
-  age: 33,
+// let myObj = {
+//   name: "Abc",
+//   age: 33,
+// };
+
+// const myFunction = function () {
+//   console.log("Hello");
+// };
+
+// ******************************************************************* //
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+// -> when stack memory is used you get a copy of the variable
+// -> when heap memory is used you get a refernce of the variable , so the original value changes.
+
+// Stack:-
+// let myName = "Sam";
+
+// let anotherName = myName;
+// anotherName = "James";
+
+// console.log(myName);
+// console.log(anotherName);
+
+// ----------------------- //
+
+// Heap:-
+let user1 = {
+  email: "user1@gmail.com",
+  upi: "user@yrl",
 };
 
-const myFunction = function () {
-  console.log("Hello");
-};
+let user2 = user1;
+
+user2.email = "user2@gmail.com";
+
+console.log(user1);
