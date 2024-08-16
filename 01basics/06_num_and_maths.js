7e7df2aeaa664cfb653f5c5753e5472dda08ca7e
shubxdoc <129772728+shubxdoc@users.noreply.github.com>
2024-08-16 17:29:06 +0000

    06 file submit

---
+++ 01basics/06_num_and_maths.js
@@ -0,0 +1,24 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNum = 123.8966;
+// console.log(otherNum.toPrecision(3));
+
+// const num = 100000000000;
+// console.log(num.toLocaleString("en-IN"));
+
+// ---------------------- MATHS ----------------------------- //
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.66));
+console.log(Math.ceil(5.66));
+console.log(Math.floor(5.66));
+console.log(Math.min(1, 2, 4, 5, 6));
+console.log(Math.max(1, 2, 4, 5, 6));
