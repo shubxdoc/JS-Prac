d3c39526686b9d1fccfb10675b1f097f28fcc785
Shubhankar <shubhankarinvisible@gmail.com>
2024-08-21 15:38:00 +0530

    02 added

--- 03basics/02_scopes.js
+++ 03basics/02_scopes.js
@@ -9,10 +9,46 @@ if (true) {
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 
 /*
 
 NOTE: global scope in browser and in code editor is differnt
 
 */
+
+// Nested Scope
+
+function one() {
+  const username = "sam";
+  function two() {
+    const website = "yt";
+    console.log(username); // sam
+  }
+  // console.log(website); // error
+  two();
+}
+
+// one();
+
+if (true) {
+  const username = "sam";
+  if (username) {
+    const website = "yt";
+    // console.log(username + website); // samyt
+  }
+  // console.log(website); //error
+}
+// console.log(username); //error
+
+// ***************** interesting *****************
+
+addone(5);
+function addone(val) {
+  return val + 1;
+}
+
+addTwo(5);
+const addTwo = function (val) {
+  return val + 2;
+};
