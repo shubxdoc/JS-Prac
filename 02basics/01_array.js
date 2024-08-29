e80a41271adea7c821384a118f571f0782bc06c2
Shubhankar <shubhankarinvisible@gmail.com>
2024-08-29 12:44:40 +0530

    add slice

--- 02basics/01_array.js
+++ 02basics/01_array.js
@@ -1,6 +1,6 @@
 //array
-let arr = [1, 3, 2, 4, 5];
-const heros = ["batman", "superman", "hangman"];
+let arr = [1, 3, 2, 4, 5]
+const heros = ["batman", "superman", "hangman"]
 const arr2 = new Array(1, 2, 3, 4)
 
 // console.log(arr2);
@@ -16,19 +16,20 @@ const arr2 = new Array(1, 2, 3, 4)
 // console.log(arr2.includes(2));
 // console.log(arr2.indexOf(2));
 
-const newArr = arr2.join()
+// const newArr = arr2.join()
 
 // console.log(newArr); // 1, 2, 3, 4 as string
 
 // slice and splice
+//-> slice(start, end)
+//-> splice(start, deleteCount)
 
-console.log("Original: ", arr2);
+console.log("Original: ", arr2)
 
 const myn1 = arr2.slice(1, 3)
-console.log('slice ' + myn1);
-console.log("Original: ", arr2);
-
+console.log("slice " + myn1) //2, 3
+console.log("Original: ", arr2)
 
 const myn2 = arr2.splice(1, 3)
-console.log('splice ' + myn2);
-console.log("Original: ", arr2);
+console.log("splice " + myn2) //2, 3, 4
+console.log("Original: ", arr2)
